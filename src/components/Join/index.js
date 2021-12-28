170285d3dbbb9c6d37ee282d9cdfdf9e192cc88c
Calvin Donner <calvinjdonner@gmail.com>
2021-12-27 21:13:55 -0700

    added files

---
+++ src/components/Join/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Join() {
+    return (
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum turpis sed ex condimentum molestie.
+        </p>
+    )
+}
+
+export default Join
---
+++ src/components/Nav/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Nav() {
+    return (
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum turpis sed ex condimentum molestie.
+        </p>
+    )
+}
+
+export default Nav
