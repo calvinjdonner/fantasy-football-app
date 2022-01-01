c1d3e33e6298209dc482e2cd4f8826d3f49967bd
Calvin Donner <calvinjdonner@gmail.com>
2022-01-01 14:44:32 -0700

    sucessfully displayed image!

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import images from '../Images/index';
+import footballImages from '../Images/index';
 
 function About() {
-        return (
-            <section className="my-5" >
-                <h1 id="about">Welcome to the Premier NFL Fantasy Football and Betting App!</h1>
-                <img src={Images.football} className="my-2" style={{ width: "100%" }} alt="cover" />
-            </section>
-        )
-    }
+    const randomImage =
+        footballImages[Math.floor(Math.random() * footballImages.length)];
+    console.log(randomImage);
+
+    return (
+        <section className="my-5" >
+            <h1 id="about">Welcome to the Premier NFL Fantasy Football and Betting App!</h1>
+            {/* <img source={randomImage} className="my-2" height={200} width={200} alt="cover" /> */}
+            <img src={require('../../assets/cover/beast.jpg')} alt="football image" />
+        </section>
+    )
+}
 
 export default About
---
+++ src/components/Images/index.js
@@ -0,0 +1,24 @@
+const footballImages = [
+    require('../../assets/cover/beast.jpg'),
+    require('../../assets/cover/butler.jpg'),
+    require('../../assets/cover/catch.jpg'),
+    require('../../assets/cover/catch2.jpg'),
+    require('../../assets/cover/dorsett.jpg'),
+    require('../../assets/cover/edelman.jpg'),
+    require('../../assets/cover/ghost.jpg'),
+    require('../../assets/cover/gleason.jpg'),
+    require('../../assets/cover/harrison.jpg'),
+    require('../../assets/cover/helmet.jpg'),
+    require('../../assets/cover/Immaculate.jpg'),
+    require('../../assets/cover/meadowlands.jpg'),
+    require('../../assets/cover/miami.jpg'),
+    require('../../assets/cover/minneapolis.jpg'),
+    require('../../assets/cover/philly.jpg'),
+    require('../../assets/cover/rodgers.jpg'),
+    require('../../assets/cover/roller.jpg'),
+    require('../../assets/cover/sea.jpg'),
+    require('../../assets/cover/swann.jpg'),
+    require('../../assets/cover/thomas.jpg')
+];
+
+export default footballImages;
