74e119812f671a2e0e68cb248482d8418152aa14
Calvin Donner <calvinjdonner@gmail.com>
2022-01-01 14:12:55 -0700

    added images to assets folder?

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import coverImage from "../../assets/cover/nfl.jpg";
+import images from '../Images/index';
 
 function About() {
-    return (
-        <section className="my-5" >
-            <h1 id="about">Welcome to the Premier NFL Fantasy Football and Betting App!</h1>
-            <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
-            <div className="my-2">
-            </div>
-        </section>
-    )
-}
+        return (
+            <section className="my-5" >
+                <h1 id="about">Welcome to the Premier NFL Fantasy Football and Betting App!</h1>
+                <img src={Images.football} className="my-2" style={{ width: "100%" }} alt="cover" />
+            </section>
+        )
+    }
 
 export default About
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,58 +1,28 @@
-import React from "react";
-import Auth from "../../utils/auth";
-import { capitalizeFirstLetter } from '../../utils/helpers';
+import React from 'react';
 
 function Nav() {
-
-    function showNavigation() {
-      if (Auth.loggedIn()) {
-        return (
-          <ul className="flex-row">
-            <li className="mx-1">
-              <Link to="/orderHistory">
-                Profile
-              </Link>
-            </li>
-            <li className="mx-1">
-              {/* this is not using the Link component to logout or user and then refresh the application to the start */}
-              <a href="/" onClick={() => Auth.logout()}>
-                Logout
-              </a>
-            </li>
-          </ul>
-        );
-      } else {
-        return (
-          <ul className="flex-row">
-            <li className="mx-1">
-              <Link to="/Join">
-                Join
-              </Link>
-            </li>
-            <li className="mx-1">
-              <Link to="/login">
-                Login
-              </Link>
-            </li>
-          </ul>
-        );
-      }
-    }
-  
     return (
-      <header className="flex-row px-1">
-        <h1>
-          <Link to="/">
-            <span role="img" aria-label="shopping bag">🛍️</span>
-            -Shop-Shop
-          </Link>
-        </h1>
-  
-        <nav>
-          {showNavigation()}
-        </nav>
-      </header>
+        <header className="flex-row px-1">
+            <h2>
+                <a href="/">
+                    <span role="img" aria-label="football">🏈</span> Premier Fantasy Football
+                </a>
+            </h2>
+            <nav>
+                <ul className="navigation">
+                    <li className="mx-2">
+                        <a href="#About">About</a>
+                    </li>
+                    <li className="mx-2">
+                        <a href="#Join">Join</a>
+                    </li>
+                    <li className="mx-2">
+                        <a href="#Schedule">Schedule</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
     );
-  }
-  
-  export default Nav;
+}
+
+export default Nav
