1a53a821b1e878aea34240e3bc4a862c63396942
Calvin Donner <calvinjdonner@gmail.com>
2022-01-04 10:51:51 -0700

    style changes

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -2,26 +2,28 @@ import React from 'react';
 
 function Nav() {
     return (
-        <header className="flex-row px-1">
-            <h2>
-                <a href="/">
-                    <span role="img" aria-label="football">🏈</span> Premier Fantasy Football
-                </a>
-            </h2>
-            <nav>
-                <ul className="navigation">
-                    <li className="mx-2">
-                        <a href="#About">About</a>
-                    </li>
-                    <li className="mx-2">
-                        <a href="#Join">Join</a>
-                    </li>
-                    <li className="mx-2">
-                        <a href="#Schedule">Schedule</a>
-                    </li>
-                </ul>
-            </nav>
-        </header>
+        <body>
+            <header>
+                <h1>
+                    <a href="/" className="premiere">
+                        <span role="img" aria-label="football">🏈</span> Premiere Fantasy Football
+                    </a>
+                </h1>
+                <nav>
+                    <ul className="navigation">
+                        <li className="mx-2">
+                            <a href="#About">About</a>
+                        </li>
+                        <li className="mx-2">
+                            <a href="#Join">Join</a>
+                        </li>
+                        <li className="mx-2">
+                            <a href="#Schedule">Schedule</a>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+        </body>
     );
 }
 
