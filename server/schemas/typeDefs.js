26d388036c442b09acdd16c5fd28750cb90a1928
Tyler <tylersundquist3@gmail.com>
2022-01-04 20:30:42 -0700

    progress on auth

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,15 +1,21 @@
-
+const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
-    type Auth {
-        token: ID!
-        user: User
-    }
+  type Auth {
+    token: ID!
+    user: User
+  }
 
-    type Mutation {
-        login(email: String!, password: String!): Auth
-        addUser(username: String!, email: String!, password: String!): Auth
-    }
+  type Query {
+    me: User
+    users: [User]
+    user(username: String!): User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+  }
 `;
 
 module.exports = typeDefs;
