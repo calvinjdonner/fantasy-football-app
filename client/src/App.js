0c4b1d279327f1344557b37eab82c576f685d4dd
Tyler <tylersundquist3@gmail.com>
2022-01-06 13:08:14 -0700

    console logging news data

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,33 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
   createHttpLink,
-} from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import Axios from "axios";
 
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Nav from "./components/Nav";
+import footballImages from "./components/Images";
 
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import Nav from './components/Nav';
-import footballImages from './components/Images';
-
-import About from './components/About';
-import Join from './components/Join';
-import Schedule from './components/Schedule';
-
+import About from "./components/About";
+import Join from "./components/Join";
+import Schedule from "./components/Schedule";
 
 const httpLink = createHttpLink({
-  uri: '/graphql',
+  uri: "/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem('id_token');
+  const token = localStorage.getItem("id_token");
   return {
     headers: {
       ...headers,
-      authorization: token ? `Bearer ${token}` : '',
+      authorization: token ? `Bearer ${token}` : "",
     },
   };
 });
@@ -39,6 +38,14 @@ const client = new ApolloClient({
 });
 
 function App() {
+  
+  const nflNews = () => {
+    Axios.get(
+      "https://api.sportsdata.io/v3/nfl/scores/json/News?key=8d47ddc73491478086570cc47316de1b"
+    ).then((response) => {
+      console.log(response);
+    });
+  };
   return (
     <ApolloProvider client={client}>
       <Router>
@@ -52,6 +59,9 @@ function App() {
             <About slides={footballImages}></About>
             <Join></Join>
             <Schedule></Schedule>
+            <div>
+              News <button onClick={nflNews}>Get NFL news now</button>
+            </div>
           </main>
         </div>
       </Router>
