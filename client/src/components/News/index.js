3b7a90e7dcc81be39d0095173dd88b5e5788b403
Tyler <tylersundquist3@gmail.com>
2022-01-06 15:35:53 -0700

    json showing

--- client/src/components/News/index.js
+++ client/src/components/News/index.js
@@ -1,71 +1,70 @@
 import React, { useEffect, useState } from "react";
-import "react-toggle/style.css"
 
-function getNews() {
-  const [post, setPost] = useState();
-  const [clicked, setClicked] = useState(false);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const url = "https://api.sportsdata.io/v3/nfl/scores/json/News?key=8d47ddc73491478086570cc47316de1b";
-    fetch(url).then(data => data.json())
-      .then(data => {
-        console.log(data);
-        setPost(data.news);
-        setLoading(false);
-      })
-  });
-
-  const toggle = index => {
-    if (clicked === index) {
-      return setClicked(null)
-    }
-
-    setClicked(index);
-  };
-  return (
-    <div className="newsSearch">
-      {!loading && post.filter((val) => {
-        return val
-      }).map((post, index) => {
-        return (
-          <section onClick={() => toggle(index)} className="container" id="card" key={index}>
-            <h4 className="newsTitle" id="title">{post.title}</h4>
-            <p className="info" id="content">{post.content}</p>
-
-          </section>
-        )
-      })}
-    </div>
-  );
-}
-
-// const apiUrl =
-//   "https://api.sportsdata.io/v3/nfl/scores/json/News?key=8d47ddc73491478086570cc47316de1b";
-
-// function News() {
-//   const [post, setPost] = React.useState(null);
+// function getNews() {
+//   const [post, setPost] = useState();
+//   const [clicked, setClicked] = useState(false);
+//   const [loading, setLoading] = useState(true);
 
 //   useEffect(() => {
-//     fetch(apiUrl)
-//       .then((data) => data.json())
-//       .then((data) => {
+//     const url = "https://api.sportsdata.io/v3/nfl/scores/json/News?key=8d47ddc73491478086570cc47316de1b";
+//     fetch(url).then(data => data.json())
+//       .then(data => {
 //         console.log(data);
-//         setPost(data);
-//       });
+//         setPost(data.news);
+//         setLoading(false);
+//       })
 //   });
-//   if (!post) return null;
 
-//   return post.map((post) => {
-//     return (
-//       <section className="container" id="card" key={post.data}>
-//         <h1 className="info-news" id="title">{post.title}</h1>
-//         <p className="news-info" id="content">{post.content}</p>
-//       </section>
-//     );
-//   });
+//   const toggle = index => {
+//     if (clicked === index) {
+//       return setClicked(null)
+//     }
+
+//     setClicked(index);
+//   };
+//   return (
+//     <div className="newsSearch">
+//       {!loading ((val) => {
+//         // return val
+//       }).map((post, index) => {
+//         return (
+//           <section onClick={() => toggle(index)} className="container" id="card" key={index}>
+//             <h4 className="newsTitle" id="title">{post.title}</h4>
+//             <p className="info" id="content">{post.content}</p>
+
+//           </section>
+//         )
+//       })}
+//     </div>
+//   );
 // }
 
-export default getNews;
+const apiUrl =
+  "https://api.sportsdata.io/v3/nfl/scores/json/News?key=8d47ddc73491478086570cc47316de1b";
+
+function News() {
+  const [post, setPost] = React.useState(null);
+
+  useEffect(() => {
+    fetch(apiUrl)
+      .then((data) => data.json())
+      .then((data) => {
+        console.log(data);
+        setPost(data);
+      });
+  });
+  if (!post) return null;
+
+  return post.map((post) => {
+    return (
+      <section className="container" id="card" key={post.data}>
+        <h1 className="info-news" id="title">{post.title}</h1>
+        <p className="news-info" id="content">{post.content}</p>
+      </section>
+    );
+  });
+}
+
+export default News;
 
 // https://api.sportsdata.io/v3/nfl/scores/json/News?key=8d47ddc73491478086570cc47316de1b
