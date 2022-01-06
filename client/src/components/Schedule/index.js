c2ffa26af85b5d42198987d9902c749d752ef103
Tyler <tylersundquist3@gmail.com>
2022-01-06 15:41:56 -0700

    teams showing on schedule

--- client/src/components/Schedule/index.js
+++ client/src/components/Schedule/index.js
@@ -1,24 +1,29 @@
-import Axios from "axios";
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+const apiUrl =
+  "https://api.sportsdata.io/v3/nfl/scores/json/ScoresByWeek/2021/18?key=8d47ddc73491478086570cc47316de1b";
 
 function Schedule() {
-  const nflSchedule = () => {
-    Axios.get(
-      "https://api.sportsdata.io/v3/nfl/scores/json/ScoresByWeek/2021/18?key=8d47ddc73491478086570cc47316de1b"
-    ).then((response) => {
-      console.log(response);
-    });
-  };
+  const [post, setPost] = React.useState(null);
 
-  return (
-    <section className="my-5">
-      <h1 id="schedule">NFL Schedule</h1>
-      <div className="my-2"></div>
-      <div>
-        NFL Schedule <button onClick={nflSchedule}></button>
-      </div>
-    </section>
-  );
+  useEffect(() => {
+    fetch(apiUrl)
+      .then((data) => data.json())
+      .then((data) => {
+        console.log(data);
+        setPost(data);
+      });
+  });
+  if (!post) return null;
+
+  return post.map((post) => {
+    return (
+      <section className="container" id="card" key={post.data}>
+        <h1 className="away-team" id="awayteam">{post.AwayTeam}</h1>
+        <p className="home-team" id="hometeam">{post.HomeTeam}</p>
+      </section>
+    );
+  });
 }
 
 export default Schedule;
