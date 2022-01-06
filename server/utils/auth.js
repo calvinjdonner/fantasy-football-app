e54edf20d3c2691d91159b68bfdf10edbe0b70ff
Tyler <tylersundquist3@gmail.com>
2022-01-05 18:33:46 -0700

    progress on auth

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -4,8 +4,12 @@ const secret = "secret";
 const expiration = "1h";
 
 module.exports = {
-  signToken: function ({ username, email, _id }) {
+  signToken: function ({ req }) {
+    let token = req.body.token || req.query.token || req.header.authorization
     const payload = { username, email, _id };
+    if (req.headers.authorization) {
+      
+    }
 
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
