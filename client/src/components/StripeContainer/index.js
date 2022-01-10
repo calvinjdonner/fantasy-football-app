8d1c49ea3aced9157f83d5f1aba7a3cedd19ad0c
Tyler <tylersundquist3@gmail.com>
2022-01-09 18:48:33 -0700

    setting up stripe donation

---
+++ client/src/components/StripeContainer/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
+const PUBLIC_KEY =
+  "pk_test_51KG8zTHw25hVTr4IgUcdnDRcyQY0L22f1BrTh12YkcW0yBjiXuGrSzXICmg1EX9Z2hvDfz6T2yzYVmVPXDOzIAzw00PtS8qTlj";
+
+const stripeTestPromise = loadStripe(PUBLIC_KEY);
+
+export default function StripeContainer() {
+    return (
+        <Elements stripe={stripeTestPromise}>
+            <PaymentForm />
+        </Elements>
+    )
+}
+
+export default StripeContainer;
